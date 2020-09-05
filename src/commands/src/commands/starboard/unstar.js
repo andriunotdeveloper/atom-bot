6b4001443e3f4cee1b6cc3cc9ecb7ee09bab3f21
andriunotdeveloper <70101656+andriunotdeveloper@users.noreply.github.com>
2020-09-05 22:49:23 +0300

    Add files via upload

---
+++ src/commands/src/commands/starboard/unstar.js
@@ -0,0 +1,76 @@
+const { Command } = require('discord-akairo');
+const Star = require('../../models/Stars');
+
+class UnstarCommand extends Command {
+	constructor() {
+		super('unstar', {
+			aliases: ['unstar'],
+			category: 'starboard',
+			channel: 'guild',
+			clientPermissions: ['MANAGE_MESSAGES'],
+			quoted: false,
+			description: {
+				content: 'Unstars a message.',
+				usage: '<message id> [channel]',
+				examples: ['396429741176913921', '396430734585233411 #OtherChannel']
+			}
+		});
+	}
+
+	*args() {
+		const channel = yield {
+			index: 1,
+			type: 'textChannel',
+			default: message => message.channel,
+			prompt: {
+				start: 'That channel could not be found. What channel is the message you are trying to remove a star from in?',
+				retry: 'Please provide a valid text channel.',
+				optional: true
+			}
+		};
+		const message = yield {
+			index: 0,
+			type: (msg, phrase) => {
+				if (!phrase) return null;
+				return channel.messages.fetch(phrase).catch(() => null);
+			},
+			prompt: {
+				start: 'What is the ID of the message you would like to remove a star from?',
+				retry: `Please provide a valid message ID in ${channel}`
+			}
+		};
+		return { message };
+	}
+
+	async exec(message, { message: msg }) {
+		if (msg.author.id === message.author.id) {
+			return message.util.channel.send('<:disagree:745003854017593354> You can\'t unstar your own message.');
+		}
+
+		const starboard = this.client.starboards.get(msg.guild.id);
+
+		if (!starboard.channel) {
+			const prefix = this.client.commandHandler.prefix(message);
+			return message.util.channel.send(`<:disagree:745003854017593354> There isn't a starboard channel to use. Set one using the \`${prefix}starboard\` command!`);
+		}
+
+		if (!message.channel.permissionsFor(this.client.user).has('MANAGE_MESSAGES')) {
+			return message.util.channel.send('<:disagree:745003854017593354> I\'m missing `Manage Messages` to unstar that message in this channel.');
+		}
+
+		const missingPerms = starboard.missingPermissions();
+		if (missingPerms) return message.util.channel.send(missingPerms);
+
+		const star = await Star.findOne({ where: { messageID: msg.id } });
+
+		if (!star || !star.starredBy.includes(message.author.id)) {
+			return message.util.channel.send('<:disagree:745003854017593354> You can\'t remove any star from this message because you never gave it one in the first place.');
+		}
+
+		const error = await starboard.remove(msg, message.author);
+		if (error) return message.util.channel.send(error);
+		return message.util.channel.send('<:agree:745003896522670100> The message has been unstarred.');
+	}
+}
+
+module.exports = UnstarCommand;
---
+++ src/commands/src/commands/tags/search.js
@@ -0,0 +1,51 @@
+const { Command } = require('discord-akairo');
+const { Op } = require('sequelize');
+const Tags = require('../../models/Tags');
+const { Util, MessageEmbed } = require('discord.js');
+
+class SearchTagCommand extends Command {
+	constructor() {
+		super('tag-search', {
+			aliases: ['tag-search'],
+			category: 'tags',
+			channel: 'guild',
+			clientPermissions: ['EMBED_LINKS'],
+			ratelimit: 2,
+			args: [
+				{
+					id: 'name',
+					match: 'content',
+					type: 'lowercase',
+					prompt: {
+						start: 'what would you like to search for?'
+					}
+				}
+			],
+			description: {
+				content: 'Searches a tag.',
+				usage: '<tag>'
+			}
+		});
+	}
+
+	async exec(message, { name }) {
+		name = Util.cleanContent(name, message);
+		const tags = await Tags.findAll({ where: { name: { [Op.like]: `%${name}%` }, guildID: message.guild.id } });
+		if (!tags.length) return message.util.channel.send(`<:disagree:745003854017593354> No results found with query ${name}.`);
+		const search = tags
+			.map(tag => `\`${tag.name}\``)
+			.sort()
+			.join(', ');
+		if (search.length >= 1950) {
+			return message.util.channel.send('<:disagree:745003854017593354> The output is way too big to display, make your search more specific and try again!');
+		}
+		const embed = new MessageEmbed()
+			.setColor("invisible")
+			.setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL())
+			.setDescription(search);
+
+		return message.util.send(embed);
+	}
+}
+
+module.exports = SearchTagCommand;
